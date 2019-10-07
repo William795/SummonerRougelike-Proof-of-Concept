cd12462c0ea37c34531fc2532fc5fdca12475d15
William Moody <oswin795@outlook.com>
2019-10-07 11:48:55 -0600

    first commit

---
+++ SummonerRougeLikeProofOfConcept/Scenes/GameScene.swift
@@ -0,0 +1,54 @@
+//
+//  GameScene.swift
+//  SummonerRougeLikeProofOfConcept
+//
+//  Created by William Moody on 10/4/19.
+//  Copyright © 2019 William Moody. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    func touchDown(atPoint pos : CGPoint) {
+        
+    }
+    
+    func touchMoved(toPoint pos : CGPoint) {
+        
+    }
+    
+    func touchUp(atPoint pos : CGPoint) {
+        
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+        
+    }
+}
