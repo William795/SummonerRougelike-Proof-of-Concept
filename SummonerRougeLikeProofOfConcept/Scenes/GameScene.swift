060b6600db6b7a7879e7559441dcfc576fe481dc
William Moody <oswin795@outlook.com>
2019-10-10 14:43:15 -0600

    finishes up most of the basics of sprite creation and movement

--- SummonerRougeLikeProofOfConcept/Scenes/GameScene.swift
+++ SummonerRougeLikeProofOfConcept/Scenes/GameScene.swift
@@ -16,6 +16,8 @@ class GameScene: SKScene {
     override func didMove(to view: SKView) {
         physicsWorld.gravity = CGVector(dx: 0, dy: 0)
         createPlayer()
+        makeSummonButton(named: "A", position: CGPoint(x: frame.maxX * 0.9, y: frame.midY / 2))
+        makeSummonButton(named: "B", position: CGPoint(x: frame.maxX * 0.8, y: frame.midY / 4))
     }
     
     func createPlayer() {
@@ -28,12 +30,52 @@ class GameScene: SKScene {
         addChild(player!)
     }
     
+    func makeSummonButton(named: String, position: CGPoint) {
+        let button = SKSpriteNode()
+        button.name = named
+        button.position = position
+        button.color = .blue
+        button.size = CGSize(width: 50, height: 50)
+        
+        addChild(button)
+    }
+    
+    func summonArcher() {
+        let minion = SKSpriteNode()
+        minion.name = "archer"
+        minion.position = player.position
+        minion.color = .green
+        minion.size = CGSize(width: 25, height: 25)
+        
+        addChild(minion)
+    }
+    
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        var pos: CGPoint!
+        
         for touch in touches {
-            self.player.run(SKAction.move(to: touch.location(in: self), duration: 0.5))
+            pos = touch.location(in: self)
         }
+        
+        let childs = self.nodes(at: pos)
+        for child in childs {
+            switch child.name {
+            case "A":
+                summonArcher()
+                return
+            case "B":
+                //do stuff
+                print("B tapped")
+                return
+            default:
+                print("didn't tap summon")
+            }
+        }
+        self.player.run(SKAction.move(to: pos, duration: 0.5))
     }
+    
+    
     
     override func update(_ currentTime: TimeInterval) {
         
